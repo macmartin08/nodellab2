c136c792effe458e56dd74521c1fa65c1393f6a4
ravi <asodariya>
2019-06-19 00:20:51 -0400

    add edit delete option

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -14,7 +14,19 @@ exports.index = (req, res) => {
 }
 
 
-exports.show = (req, res) => {}
+exports.show = (req, res) => {
+  Student.findById(
+    req.params.id)
+  .then(student => {
+    res.render('student/show',{
+      student: student,
+      title: student.firstname
+    });
+  })
+  .catch(err => {
+      console.error(`ERROR: ${err}`);
+    });
+}
 
 
 exports.new = (req, res) => {
@@ -32,4 +44,45 @@ exports.create = (req, res) => {
     .catch(err => {
       console.error(`ERROR: ${err}`);
     });
+
+
 }
+
+exports.edit = (req, res) => {
+  Student.findById(req.params.id)
+    .then(student => {
+      res.render('student/edit', {
+        title: `Edit ${student.firstname}`,
+        student: student
+      })
+    })
+    .catch(err => {
+      console.error(`ERROR: ${err}`);
+    });
+};
+
+
+exports.update = (req, res) => {
+  console.log("done")
+  Student.updateOne(req.body.id
+    , req.body.student, {
+      runValidators: true
+    })
+    .then(() => {
+      res.redirect('/student');
+    })
+    .catch(err => {
+      console.error(`ERROR: ${err}`);
+    });
+};
+
+
+exports.destroy = (req, res) => {
+  Student.deleteOne(req.body.id)
+    .then(() => {
+      res.redirect('/student');
+    })
+    .catch(err => {
+      console.error(`ERROR: ${err}`);
+    });
+};
--- routes/student.js
+++ routes/student.js
@@ -5,9 +5,9 @@ const StudentController =require('../controllers/studentController')
 router.get(`/`, StudentController.index);
 router.get(`/new`, StudentController.new);
 router.get(`/:id`, StudentController.show);
-//router.get(`/:id/edit`, StudentController.edit);
+router.get(`/:id/edit`, StudentController.edit);
 router.post(`/`, StudentController.create);
-//router.post(`/update`, StudentController.update);
-//router.post(`/destroy`, StudentController.destroy);
+router.post(`/update`, StudentController.update);
+router.post(`/destroy`, StudentController.destroy);
 
 module.exports = router;
