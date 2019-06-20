21498f1f09a49c3b667e9e12665364632b5c6b7a
ravi <asodariya>
2019-06-19 23:18:58 -0400

    add about page

--- routes/pages.js
+++ routes/pages.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const PagesController = require('../controllers/PagesController');
 // Our routes
 router.get(`/`, PagesController.show);
+router.get(`/about`, PagesController.show);
 // We have to export our changes
 module.exports = router;
